c2072e91b225a1bb980e13460e0f20ef8a4a3a43
Lavender <444049406@qq.com>
2020-03-22 09:48:27 +0800

    2ed commit for function of init_list

--- Double list.c
+++ Double list.c
@@ -15,3 +15,23 @@ DouList *set_order_list();                 // 以指定元素建立一个递增表
 void setify(DouList **header);             // 集合化链表
 int isInList(DouList *, int);              // 判断元素是否在链表中
 void del_value(DouList *header, int data); // 删除所有指定元素
+
+DouList *init_list(int n)
+{
+    DouList *header = (DouList *)malloc(sizeof(DouList));
+    DouList *p = header;
+    header->left = header;
+    header->right = header;
+    for (int i = 0; i < n; i++)
+    {
+        DouList *newNode = (DouList *)malloc(sizeof(DouList));
+        p->right = newNode;
+        newNode->data = 0;
+        newNode->left = p;
+        newNode->right = header;
+        p = p->right;
+        header->left = newNode;
+    }
+    header->data = n;
+    return header;
+}
